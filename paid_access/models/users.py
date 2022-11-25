cbc5c48898042d6d65c985ec137826ab3d47f787
Denis Smirnov <w1png@yandex.ru>
2022-11-25 21:27:46 +0300

    Add users model

---
+++ paid_access/models/users.py
@@ -0,0 +1,27 @@
+from typing import Any
+from utils import database
+import promocodes
+
+def get_database_table() -> str:
+    return """CREATE TABLE IF NOT EXISTS users (
+    id INTEGER,
+    promocode FOREIGN KEY REFERENCES promocodes(id),"""
+
+
+class User:
+    def __init__(self, user_id: int) -> None:
+        self.user_id = user_id
+
+    def __query(self, field: str) -> Any:
+        return database.fetch(f"SELECT {field} FROM users WHERE id = ?", (self.user_id,))
+
+    def __update(self, field: str, value: str) -> None:
+        database.execute(f"UPDATE users SET {field} = ? WHERE id = ?", (value, self.user_id))
+
+    @property
+    def promocode(self) -> promocodes.Promocode:
+        return promocodes.Promocode(self.__query("promocode"))
+    @promocode.setter
+    def promocode(self, value: promocodes.Promocode) -> None:
+        self.__update("promocode", value.id)
+
