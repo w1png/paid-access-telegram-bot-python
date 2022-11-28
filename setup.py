c494e361940bff1f5f5ad2f023129b1b94203474
Denis Smirnov <w1png@yandex.ru>
2022-11-28 21:13:19 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os, json
+
+def create_config():
+    default_config = {
+        "main_admin_id": input("Введите ID главного администратора: "),
+        "info": {
+            "greeting": "Привет, я бот-продавец. Чтобы узнать, что я умею, напиши /help",
+        },
+    }
+
+    open("config.json", "w").write(json.dumps(default_config, indent=4, encoding="utf-8"))
+
+
+if not os.path.exists("config.json"):
+    create_config()
+elif input("config.json уже существует. Вы уверены, что хотите перезаписать его? Все данные будут потеряны. (y/n)").lower() == "y":
+    create_config()
+else:
+    print("Пропускаем создание config.json")
+
+if os.path.exists("database.db") and input("Вы хотите перезаписать базу данных? Все данные будут потеряны. (y/n)").lower() == "y":
+    os.remove("database.db")
+    
