48a7556ac02450c12b4ca253e4801d8208ded254
Denis Smirnov <w1png@yandex.ru>
2022-11-29 07:33:51 +0300

    Allow no args

--- src/utils/database.py
+++ src/utils/database.py
@@ -1,15 +1,15 @@
 import sqlite3
 from typing import Any
 
-def fetch(query, args) -> Any:
+def fetch(query, args=None) -> Any:
     with sqlite3.connect("database.db") as connection:
         cursor = connection.cursor()
-        cursor.execute(query, args)
+        cursor.execute(query, args or ())
         return cursor.fetchall()
 
-def execute(query, args) -> None:
+def execute(query, args=None) -> None:
     with sqlite3.connect("database.db") as connection:
         cursor = connection.cursor()
-        cursor.execute(query, args)
+        cursor.execute(query, args or ())
         connection.commit()
 
