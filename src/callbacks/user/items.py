3e1ac8ec67e47763271dc69e984aa88d67303537
Denis Smirnov <w1png@yandex.ru>
2022-11-29 07:33:59 +0300

    Add items

---
+++ src/callbacks/user/items.py
@@ -0,0 +1,23 @@
+import models
+
+from aiogram import types
+import markups
+from constants import language
+
+
+async def execute(user: models.users.User, query: types.CallbackQuery | types.Message, data: dict):
+    message = query
+    if not isinstance(query, types.Message):
+        message = query.message
+
+    markup = [
+            (item.name, f"user_item{'i': item.id}")
+        for item in models.items.get_items()
+    ]
+    markup.append((language.close, "user_close"))
+
+    return await message.answer(
+        text=language.items,
+        reply_markup=markups.create_markup(markup)
+    )
+
